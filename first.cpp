5f4b63d1722dc5cc95b92a632a3cd9c264a2c694
tonmoy-y <tonmoy4451@gmail.com>
2025-09-10 11:35:55 +0600

    v 2.0.0

--- first.cpp
+++ first.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
-bool isPrime(int n) {
+bool isPrime(long long n) {
     if (n <= 1) return false;
-    for (int i = 2; i * i <= n; ++i) {
-        if (n % i == 0) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (long long i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
     }
     return true;
 }
 
-int main() {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
+int main(int argc, char** argv) {
+    long long num;
+    if (argc >= 2) {
+        try {
+            num = stoll(argv[1]);
+        } catch (...) {
+            cout << "Invalid number.\n";
+            return 1;
+        }
+    } else {
+        cout << "Enter a number: ";
+        while (!(cin >> num)) {
+            cout << "Please enter a valid integer: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
 
     if (isPrime(num))
-        cout << num << " is a prime number." << endl;
+        cout << num << " is a prime number.\n";
     else
-        cout << num << " is not a prime number." << endl;
+        cout << num << " is not a prime number.\n";
 
     return 0;
 }
